d1c2b5a60965e01edb0ff97fe41a206f94181ae9
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-01-22 21:01:52 -0500

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace lab2
+{
+    class StockItem
+    {
+        private String description;
+        static int serial;
+        private int ID;
+        private double price;
+        private int quantity;
+
+        public StockItem()
+        {
+            description = "";
+            ID = 0;
+            price = 0.00;
+            quantity = 0;
+            ID =+ serial++;
+        } //default
+
+        public StockItem(String desc, double sale, int quantity)
+        {
+            ID = serial++;
+            description = desc;
+            price = sale;
+            this.quantity = quantity;
+            
+
+        } //overloaded
+
+        public override String ToString()
+        {
+            return  ": Item number: " + ID + " is " + description + " has price of " + price + " we currently have " + quantity;
+        }
+        // Accessors follow
+        public String getD()
+        {
+            return description;
+        }
+        public int getID()
+        {
+            return ID;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        // Mutators follow
+        public double newPrice(double a)
+        {
+            if (a >= 0.00)
+            {
+                price = a;
+                return price;
+            }
+            else
+            {
+                Console.Write("Error");
+                return price;
+            }
+
+        }
+        public int lowQuantity(int a)
+        {
+            if (quantity - a >= 0)
+            {
+                quantity -= a;
+                return quantity;
+            }
+            else
+            {
+                Console.Write("Error");
+                return quantity;
+            }
+        }
+        public int addQuantity(int a)
+        {
+            quantity += a;
+            return quantity;
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace lab2;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        StockItem milk = new StockItem("1 Gallon of Milk", 3.60, 15);
+        StockItem bread = new StockItem("1 Loaf of Bread", 1.98, 30);
+        int choice;
+        do
+        {
+            Console.WriteLine("1. Sold One Milk");
+            Console.WriteLine("2. Sold One Bread");
+            Console.WriteLine("3. Change Price of Milk");
+            Console.WriteLine("4. Change Price of Bread");
+            Console.WriteLine("5. Add Milk to Inventory");
+            Console.WriteLine("6. Add Bread to Inventory");
+            Console.WriteLine("7. See Inventory");
+            Console.WriteLine("8. Quit");
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                milk.lowQuantity(1);
+                
+            } else if(choice == 2)
+            {
+                bread.lowQuantity(1);
+            } else if(choice == 3)
+            {
+                Console.WriteLine("Enter new Price of milk below:");
+                double sale = double.Parse(Console.ReadLine());
+                milk.newPrice(sale);
+            } else if (choice == 4)
+            {
+                Console.WriteLine("Enter new Price of bread below:");
+                double sale = double.Parse(Console.ReadLine());
+                milk.newPrice(sale);
+            } else if (choice == 5) {
+                Console.WriteLine("Enter the shipment of milk recieved below:");
+                int shipment = int.Parse(Console.ReadLine());
+                milk.addQuantity(shipment);
+            } else if (choice == 6)
+            {
+                Console.WriteLine("Enter the shipment of bread recieved below:");
+                int shipment = int.Parse(Console.ReadLine());
+                bread.addQuantity(shipment);
+            } else if (choice == 7)
+            {
+                Console.WriteLine("milk" + milk);
+                Console.WriteLine("bread" + bread);
+            } else if (choice > 8)
+            {
+                Console.WriteLine("Enter valid operation");
+            } 
+            
+        }
+        while (choice != 8);
+        
+    }
+}
+
+
+    
