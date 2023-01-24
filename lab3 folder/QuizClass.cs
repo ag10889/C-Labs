23848a0c4096a3ee4193c0dc51ffa71ada94b521
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-01-24 13:49:13 -0500

    Add files via upload

---
+++ lab3 folder/QuizClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+namespace lab3
+{
+	public class QuizClass
+	{
+		List<question> questionBank = new List<question>();
+        
+
+        public void add_question(String questionText, String answer, int diff)
+		{
+            question a = new question(questionText, answer, diff);
+            questionBank.Add(a);
+		}
+		public void remove_question()
+		{
+			foreach (var question in questionBank)
+			{
+				Console.WriteLine(question);
+			}
+			Console.WriteLine("Which question would you like to remove?");
+			String remove = Console.ReadLine();
+
+		}
+		
+	}
+}
+
