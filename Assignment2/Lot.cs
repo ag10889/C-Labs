3756e338837000302ebc306f405d3b4a2cb6876a
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-01-31 11:05:42 -0500

    Add files via upload

---
+++ Assignment2/Lot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Assignment2
+{
+	public class Lot
+	{
+		private int lotNumber = 1000;
+		private String description;
+		private int currentBid;
+		private int bidIncrement;
+		private bool sold;
+		public Lot()
+		{
+			lotNumber ++;
+			description = "Unknown Item";
+			currentBid = 0;
+			bidIncrement = 0;
+			sold = false;
+
+		}
+		public Lot(String A, int B, int C)
+		{
+			lotNumber ++;
+			description = A;
+			currentBid = B;
+			bidIncrement = C;
+			sold = false;
+		}
+		public void markSold()
+		{
+			sold = true;
+		}
+		public bool getSold()
+		{
+			return sold;
+		}
+		public int getBidIn()
+		{
+			return bidIncrement;
+		}
+		public String getDes()
+		{
+			return description;
+		}
+		public void setBid(int A)
+		{
+			currentBid = A;
+		}
+		public int nextBid()
+		{
+			return currentBid + bidIncrement;
+		}
+		public override String ToString()
+		{
+			if (sold == true)
+			{
+                return "Lot " + lotNumber + ". " + description + " was sold for " + currentBid;
+            } else
+			{
+				return "Lot " + lotNumber + ". " + description + " current bid " + currentBid + " minimum bid " + nextBid(); 
+			}
+			
+		}
+
+	}
+}
+
