e58fe9f2925c0b6ec6d93c831e05e2483f9bb762
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-02-22 14:02:54 -0500

    Add files via upload

---
+++ Lab7/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Lab7
+{
+	public class Calculator : CalcOp
+	{
+		public Calculator()
+		{
+            
+		}
+        public int Add(int a, int b)
+        {
+            Console.Write("These two numbers added equal: ");
+            Console.WriteLine(a + b);
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            Console.Write("These two numbers subtracted from each other equal: ");
+            Console.WriteLine(a - b);
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            Console.Write("These two numbers multiply to equal: ");
+            Console.WriteLine(a * b);
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            Console.Write("These two numbers divided equal: ");
+            Console.WriteLine(a / b);
+            return a / b;
+        }
+    }
+}
+
---
+++ Lab7/Driver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Lab7 {
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Calculator calc = new Calculator();
+            int choice;
+            do
+            {
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("1 - Addition");
+                Console.WriteLine("2 - Subtraction");
+                Console.WriteLine("3 - Multiplication");
+                Console.WriteLine("4 - Division");
+                choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("Please enter the two numbers you would like to add");
+                    int I = int.Parse(Console.ReadLine());
+                    int A = int.Parse(Console.ReadLine());
+                    calc.Add(I, A);
+
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Please enter the two numbers you would like to subtract");
+                    int I = int.Parse(Console.ReadLine());
+                    int A = int.Parse(Console.ReadLine());
+                    calc.Subtract(I, A);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Please enter the two numbers you would like to multiply");
+                    int I = int.Parse(Console.ReadLine());
+                    int A = int.Parse(Console.ReadLine());
+                    calc.Multiply(I, A);
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("Please enter the two numbers you would like to divide");
+                    int I = int.Parse(Console.ReadLine());
+                    int A = int.Parse(Console.ReadLine());
+                    calc.Divide(I, A);
+                }
+                else if (choice > 4)
+                {
+                    Console.WriteLine("Enter valid operation");
+                }
+
+            }
+            while (choice != 0);
+        }
+    }
+}
