b1754e3f2d31689d58524dda107864f327ccf1e8
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-03-14 11:11:27 -0400

    Add files via upload

---
+++ Lab8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Recursion2
+{
+    static string repeatNTimes(int num, string sentence)
+    {
+        if (num == 0)
+        {
+            return " ";
+        }
+        else
+        {
+
+            return sentence +  repeatNTimes(num - 1, sentence);
+        }
+     }
+    static bool isReverse(string A, string B)
+    {
+        
+        return false;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(repeatNTimes(4, "Hi"));
+        Console.WriteLine(isReverse("Hello", "olleH"));
+    }
+}
+
