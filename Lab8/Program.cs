179650d3ad1bedafb9f5de0f49b72cf3922f5ff0
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-03-14 16:56:25 -0400

    Add files via upload

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -13,10 +13,38 @@ public class Recursion2
             return sentence +  repeatNTimes(num - 1, sentence);
         }
      }
+    static string removeAt( string sentence) {
+        return sentence.Substring(1, sentence.Length-1);
+    }
+
     static bool isReverse(string A, string B)
     {
+        if (A.Length != B.Length)
+        {
+            return false;
+        }
+        else if (A == "" && B == "")
+        {
+            return true;
+        }
+        else if (A.Length == 1 && B.Length == 1)
+        {
+            return A[0] == B[0];
+        }
+        else
+        {
+            if (A[0] == B[B.Length - 1])
+            {
+                A = A.Substring(1);
+                B = B.Substring(0, B.Length - 1);
+                return isReverse(A, B);
+            }
+            else
+            {
+                return false;
+            }
+        }
         
-        return false;
     }
     public static void Main(string[] args)
     {
